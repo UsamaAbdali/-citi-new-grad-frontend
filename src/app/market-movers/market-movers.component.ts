8ff943b2de1d8495f315a82696646b11962763a5
Usama <usama01@my.yorku.ca>
2021-08-25 00:07:14 -0400

    finished marketmover but something broke

--- src/app/market-movers/market-movers.component.ts
+++ src/app/market-movers/market-movers.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 // import {Account} from '../model/account';
-// import { Securities } from '../model/Securities';
-import { AccountServiceService } from '../service/account-service.service';
+import { Securities } from '../model/Securities';
+// import { AccountServiceService } from '../service/account-service.service';
 import { SercuritiesService } from '../service/sercurities.service';
 
 
@@ -24,12 +24,12 @@ export class MarketMoversComponent implements OnInit {
 
   ngOnInit() {
     this.setTopGainers();
-    
+    this.setTopLosers();
+
   }
 
   setTopGainers(){
-    this.securitiesService.getTopFiveGainers().subscribe(data =>{
-      // this.topGainersSecurities=
+    this.securitiesService.getTopGainers().subscribe(data =>{
       data.forEach(security => {
         this.topGainersSecurities.push(security);
       });
@@ -37,6 +37,28 @@ export class MarketMoversComponent implements OnInit {
     error =>{
       console.log("ERROR: Couldn't get Top 5 Gainers",error);
     }
+    console.log("topGainersSecurities",this.topGainersSecurities);
+  }
+
+  setTopLosers(){
+    this.securitiesService.getTopLosers().subscribe(data =>{
+      data.forEach(security => {
+        this.topLosersSecurities.push(security);
+      });
+    }),
+    error =>{
+      console.log("ERROR: Couldn't get Top 5 Losers",error);
+    }
+    console.log("topLosersSecurities",this.topLosersSecurities);
+  }
+
+  securityChangeCalculator(security:Securities):number{
+    // Calculate percentage change
+    var percentChange=((security.current_cost-security.closing_cost)/security.closing_cost)*100;
+    
+    //Round to 3 decimals
+    percentChange=Math.round(percentChange * 1000) / 1000;
+    return percentChange;
   }
 
 }
--- src/app/service/sercurities.service.ts
+++ src/app/service/sercurities.service.ts
@@ -8,16 +8,23 @@ export class SercuritiesService {
   
   private baseUrl: string;
   private topFiveGainsUrl: string;
+  private topFiveLosersUrl: string;
+
 
   // http://portfolio-project-portfolio-project.namdevops24.conygre.com/account/dailyGainers
 
   constructor(private http: HttpClient) { 
     this.baseUrl="http://portfolio-project-portfolio-project.namdevops24.conygre.com"; 
     this.topFiveGainsUrl = this.baseUrl+"/account/dailyGainers";
+    this.topFiveLosersUrl = this.baseUrl+"/account/dailyLosers";
   }
 
-  public getTopFiveGainers(): Observable<Securities[]> {
+  public getTopGainers(): Observable<Securities[]> {
     return this.http.get<Securities[]>(this.topFiveGainsUrl);
+  }
+
+  public getTopLosers(): Observable<Securities[]> {
+    return this.http.get<Securities[]>(this.topFiveLosersUrl);
   }
 
 }
